0017ce44f3fcf7d830b1f9bb9e1c6d1f748a898a
jonghyouk yun <ageldama@gmail.com>
2026-05-25 12:15:08 +0900

    --wip-- [skip ci]

--- src/main.c
+++ src/main.c
@@ -9,9 +9,34 @@ main(int argc, char* argv[])
   (void)argc;
   (void)argv;
 
-  // TODO
+
 
 
   exit(EXIT_SUCCESS);
 }
 
+
+
+
+#if 0
+/*
+
+--- TODOs ------------------------------------------------------------
+
+* arg-parsing
+
+* save/load db-file : dict(k:str/v:int) dict(k:str/v:[]int)
+
+* rofi : select list, ask-yn
+
+* execvp
+
+* regex matching
+
+* dirent find files
+
+
+--- DONEs ------------------------------------------------------------
+
+*/
+#endif
