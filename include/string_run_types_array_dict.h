025d52e761e2ac5e6e3da157ccc505d45623af48
jonghyouk yun <ageldama@gmail.com>
2026-05-25 15:08:41 +0900

    [WIP] db

--- include/string_run_types_array_dict.h
+++ include/string_run_types_array_dict.h
@@ -7,9 +7,11 @@
 #include "m-string.h"
 
 // => string_run_types_array_dict_t
-DICT_DEF2(string_run_types_array_dict, string_t, M_STRING_OPLIST, run_types_array_t, M_OPL_run_types_array_t())
+DICT_DEF2(string_run_types_array_dict, string_t, M_STRING_OPLIST,
+    run_types_array_t, M_OPL_run_types_array_t())
 
-#define M_OPL_string_run_types_array_dict_t() DICT_OPLIST(string_run_types_array_dict, M_STRING_OPLIST, M_OPL_run_type_array_t())
-
+#define M_OPL_string_run_types_array_dict_t()                        \
+    DICT_OPLIST(string_run_types_array_dict, M_STRING_OPLIST,        \
+        M_OPL_run_type_array_t())
 
 #endif /* STRING_RUN_TYPES_ARRAY_DICT_H */
--- src/db.c
+++ src/db.c
@@ -3,15 +3,14 @@
 string_time_dict_t db_v_cmd_last_epochs;
 string_run_types_array_dict_t db_v_cmd_run_types;
 
-
 void db_init()
 {
-  //    string_time_dict_init(db_v_cmd_last_epochs);
-  //    string_run_types_array_dict_init(db_v_cmd_run_types);
+    //    string_time_dict_init(db_v_cmd_last_epochs);
+    //    string_run_types_array_dict_init(db_v_cmd_run_types);
 }
 
 void db_cleanup()
 {
-  //    string_time_dict_clear(db_v_cmd_last_epochs);
-  //    string_run_types_array_dict_clear(db_v_cmd_run_types);
+    //    string_time_dict_clear(db_v_cmd_last_epochs);
+    //    string_run_types_array_dict_clear(db_v_cmd_run_types);
 }
