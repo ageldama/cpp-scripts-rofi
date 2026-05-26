bc3d8fb5379be4d2b1f76d94b61388337d120850
jonghyouk yun <ageldama@gmail.com>
2026-05-26 17:17:59 +0900

    [WIP] main

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <memory>
 #include <optional>
+#include <stdexcept>
 #include <unistd.h>
 
 #include "argp.hpp"
 #include "db.hpp"
+#include "exec.hpp"
 #include "file_find.hpp"
 #include "rofi.hpp"
 #include "str.hpp"
@@ -36,8 +38,7 @@ int main(int argc, char* argv[])
         SR::db::load(SR::argp::v_db_file.c_str());
 
     if (SR::argp::v_dump_and_exit) {
-        // TODO print(db)
-        exit(EXIT_SUCCESS);
+        throw std::invalid_argument("TODO print(db)");
     }
 
     //
@@ -62,15 +63,35 @@ int main(int argc, char* argv[])
     const auto final_run_type = final_run_type_.value();
 
     if (SR::argp::db_save_allowed()) {
-          SR::db::upd_last_epoch(cmd);
-          SR::db::incr_run_count(cmd, final_run_type);
-          SR::db::save(SR::argp::v_db_file.c_str());
+        SR::db::upd_last_epoch(cmd);
+        SR::db::incr_run_count(cmd, final_run_type);
+        SR::db::save(SR::argp::v_db_file.c_str());
     }
 
-    // TODO print
-    // TODO exec
-    // TODO exec-wrapper
-    // TODO exec-in-term
+    auto cmdv = SR::string_vector {
+        cmd,
+    };
+
+    if (SR::argp::v_exec_wrapper != "") {
+        // TODO vec.insert(vec.begin(), prefix.begin(), prefix.end());
+    }
+
+    if (final_run_type == SR::db::RUN_IN_TERM) {
+        // TODO
+    }
+
+    if (SR::argp::v_print) {
+        for (const auto& cmd : cmdv) {
+            std::cout << cmd << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    if (SR::argp::v_execute) {
+        const auto err_msg = SR::exec::execvp(cmdv);
+        // TODO rofi -e "..."
+        exit(EXIT_FAILURE);
+    }
 
     exit(EXIT_SUCCESS);
 }
