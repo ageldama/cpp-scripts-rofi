1a93b66a7f0c0c70348eb9ed4649f1a7100613e4
jonghyouk yun <ageldama@gmail.com>
2026-05-26 17:21:52 +0900

    [WIP] main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -94,68 +93,4 @@ int main(int argc, char* argv[])
     }
 
     exit(EXIT_SUCCESS);
-}
-
-std::optional<SR::db::run_type_t> ask_most_run_type(
-    const std::string& cmd, const bool alt)
-{
-    const auto most_run_type
-        = SR::db::get_most_run_type(cmd, SR::db::RUN_NORMAL);
-
-    const auto selected_run_type
-        = alt ? SR::db::RUN_IN_TERM : SR::db::RUN_NORMAL;
-
-    SR::db::run_type_t final_run_type = SR::db::RUN_NORMAL;
-
-    if (selected_run_type != most_run_type) {
-        auto opts_yn = SR::rofi::rofi_common_opts {
-            .prompt
-            = "Different run type from usual, Correct it usually?",
-            .ignorecase = SR::argp::v_ignorecase,
-        };
-        const std::string run_normal("No Terminal");
-        const std::string run_in_term("In terminal");
-        const auto lbl_y = most_run_type == SR::db::RUN_NORMAL
-            ? run_normal
-            : run_in_term;
-        const auto lbl_n = most_run_type == SR::db::RUN_NORMAL
-            ? run_in_term
-            : run_normal;
-        auto res_yn = SR::rofi::ask_yn(opts_yn, lbl_y, lbl_n);
-        if (res_yn) {
-            if (res_yn.value().stdout == "0") {
-                final_run_type = most_run_type;
-            } else {
-                final_run_type = selected_run_type;
-            }
-        } else {
-            return std::nullopt;
-        }
-    }
-
-    return std::make_optional(final_run_type);
-}
-
-SR::string_vector sorted_file_list()
-{
-    auto files = string_vector {};
-
-    if (SR::argp::v_file_regexes.size() > 0) {
-        files
-            = file_find::find_in_directories(SR::argp::v_script_dirs,
-                file_find::make_only_file_and_regex_match(
-                    SR::argp::v_file_regexes));
-    } else {
-        files = file_find::find_in_directories(
-            SR::argp::v_script_dirs, file_find::only_file);
-    }
-
-    std::sort(files.begin(), files.end(),
-        [](const std::string& a, const std::string& b) {
-            const auto a_epoch = SR::db::get_last_epoch(a);
-            const auto b_epoch = SR::db::get_last_epoch(b);
-            return a_epoch > b_epoch;
-        });
-
-    return files;
 }
